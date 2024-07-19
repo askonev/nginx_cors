5d82e7a6e9b047bcc7637734ef7e0986c13ef8d7
MaxSorokin <askonev007@gmail.com>
2024-07-19 19:02:54 +0300

    ref: min

--- html/db_scripts/CDE/buidlerJS_argument/builderJS_via_GV.js
+++ html/db_scripts/CDE/buidlerJS_argument/builderJS_via_GV.js
@@ -1,18 +1,20 @@
 
-// db --argument=\{\"file\"\:\"/home/askonew/Downloads/chrome.tmp/new.docx\"\,\"name\"\:\"new.docx\"\}
+// odb --argument=\{\"file\"\:\"/home/askonew/Downloads/chrome.tmp/new.docx\"\,\"name\"\:\"new.docx\"\}
 
-builderJS.CreateFile("docx");
+// builderJS.CreateFile("docx");
+builder.CreateFile("docx");
     var sFile = Argument["file"];
-    // console.log('Arg:')
-    // console.log(sFile)
-    GlobalVariable["sFile"] = sFile;
+    console.log('Arg:')
+    console.log(sFile)
+    // GlobalVariable["sFile"] = sFile;
     // console.log('GV:')
     // console.log(GlobalVariable["sFile"]);
-builderJS.CloseFile();
+// builderJS.CloseFile();
+builder.CloseFile();
 
 // builderJS.OpenFile(GlobalVariable["sFile"]);
-builderJS.CreateFile("docx")
-    console.log('Result:')
-    console.log(GlobalVariable["sFile"]);
-// builderJS.SaveFile("docx", "new.docx")
-builderJS.CloseFile();
+// builderJS.CreateFile("docx")
+//     console.log('Result:')
+//     console.log(GlobalVariable["sFile"]);
+// // builderJS.SaveFile("docx", "new.docx")
+// builderJS.CloseFile();
