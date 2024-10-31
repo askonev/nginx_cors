0c5397e5f038feaf2f56f2c50f14f8951f383fb4
MaxSorokin <askonev007@gmail.com>
2024-10-31 14:09:06 +0300

    feature: jpg.comparer

--- html/db_scripts/CDE/jpeg_compare.js
+++ html/db_scripts/CDE/jpeg_compare.js
@@ -1,6 +1,6 @@
 builderJS.OpenFile("http://192.168.4.138:9090/files/empty.docx");
-    var file = builderJS.OpenTmpFile("http://192.168.4.138:9090/files/docx/docx_with_jpg.docx");
+    var file = builderJS.OpenTmpFile("http://192.168.4.138:9090/files/docx/jpg.docx");
         AscCommonWord.CompareDocuments(Api, file, null);
         file.Close();
-builderJS.SaveFile("docx", "Result.docx");
+builderJS.SaveFile("docx", "result.docx");
 builderJS.CloseFile();
